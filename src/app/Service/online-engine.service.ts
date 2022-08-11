7dc1190eaf744f0f3fea87be0a109408f3b43e7f
amit mishra <amit.mishra@gridinfocom.com>
2022-08-11 17:58:37 +0530

    Api integration

--- src/app/Service/online-engine.service.ts
+++ src/app/Service/online-engine.service.ts
@@ -5,7 +5,7 @@ import { delay, map, tap } from 'rxjs/operators';
 import { AppConfig } from './app-config.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InvoiceService {
   invoiceUrl: string = '';
@@ -14,13 +14,33 @@ export class InvoiceService {
     this.invoiceUrl = config.invoiceUrl;
   }
 
-  uploadInvoice(obj:any) : Observable<any>{
-    return this.http.post<any>(`${this.invoiceUrl}submit`,obj)
+  uploadInvoice(obj: any): Observable<any> {
+    return this.http.post<any>(`${this.invoiceUrl}submit`, obj);
   }
 
-  getDataByReferenceId(obj: any) : Observable<any>{
-       return this.http.post<any>(`${this.invoiceUrl}get/filter`,obj);
+  getDataByReferenceId(obj: any): Observable<any> {
+    return of({
+      success: true,
+
+      successMsg: 'Response Received successfully',
+
+      successCode: 'SUCCESS_CODE_200',
+
+      response: [
+        {
+          referenceId: '150592192112',
+
+          responseJson:
+            '{"referenceId": "AXP349F70UIK", "DocumentData": {"Date": "", "VendorName": "", "BillingName": "", "TotalAmount": "", "InvoiceNumber": ""}, "ApplicationData": {"Date": "", "VendorName": "", "BillingName": "", "TotalAmount": "", "InvoiceNumber": ""}, "requstProcessorId": 6, "DocumentMatchedData": {"Date": true, "VendorName": true, "BillingName": false, "TotalAmount": true, "InvoiceNumber": true}}',
+
+          status: 'success',
+
+          requestTimestamp: '2022-08-08T09:34:46.138+00:00',
+
+          responseTimestamp: '2022-08-08T11:18:48.367+00:00',
+        },
+      ],
+    });
+    return this.http.post<any>(`${this.invoiceUrl}get/filter`, obj);
   }
-
-
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,21 @@
 import { formatDate } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
-import { FileRestrictions, RemoveEvent, SelectEvent } from '@progress/kendo-angular-upload';
+import {
+  FileRestrictions,
+  RemoveEvent,
+  SelectEvent,
+} from '@progress/kendo-angular-upload';
 import * as moment from 'moment';
 import { AppConfig } from '../Service/app-config.service';
-import { orderBy, process, SortDescriptor } from "@progress/kendo-data-query";
+import { orderBy, process, SortDescriptor } from '@progress/kendo-data-query';
 import * as XLSX from 'xlsx';
 import { Router } from '@angular/router';
 import { LoginService } from '../Service/LogIn/login.service';
@@ -15,14 +24,14 @@ import { InvoiceService } from '../Service/online-engine.service';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
   headerStyle = {
     'background-color': '#004d94',
     color: '#fff',
     'font-size': '12px',
-    'padding-left':'5px'
+    'padding-left': '5px',
   };
   data = [];
   public sheetDataToUpload: any;
@@ -53,25 +62,28 @@ export class FormComponent implements OnInit {
   minDate = new Date();
   maxDate = new Date();
 
-
   message: any;
-  loading: boolean = false
+  loading: boolean = false;
   selectedTab: number = 0;
 
   openAlertDialog: boolean = false;
   successMsg: string = '';
   successTitle: string = '';
   errorList = [];
-  errorMap:any = null;
+  errorMap: any = null;
   objectKeys = Object.keys;
 
-  constructor(private router: Router, private _invoiceService: InvoiceService, private loginobj: LoginService) { }
+  constructor(
+    private router: Router,
+    private _invoiceService: InvoiceService,
+    private loginobj: LoginService
+  ) {}
 
-  allData: any = []
+  allData: any = [];
   openedDialog: boolean = false;
   inputDataObj: any;
   title: any;
-  stateName: any = []
+  stateName: any = [];
 
   filterForm: FormGroup = new FormGroup({
     fromDate: new FormControl(new Date()),
@@ -79,55 +91,60 @@ export class FormComponent implements OnInit {
     referenceId: new FormControl(''),
   });
 
- year= new Date().getFullYear();
+  year = new Date().getFullYear();
 
   currentUserName: any = null;
   loggedInUserDetails: any = null;
 
   ngOnInit(): void {
-    this.minDate = new Date(this.minDate.setFullYear(this.minDate.getFullYear() - 85));
-    this.maxDate = new Date(this.maxDate.setFullYear(this.maxDate.getFullYear() + 15));
+    this.minDate = new Date(
+      this.minDate.setFullYear(this.minDate.getFullYear() - 85)
+    );
+    this.maxDate = new Date(
+      this.maxDate.setFullYear(this.maxDate.getFullYear() + 15)
+    );
     this.getUserDetails();
   }
 
-  getUserDetails(){
+  getUserDetails() {
     this.loggedInUserDetails = sessionStorage.getItem('userDetails');
     //console.log('user details', this.loggedInUserDetails)
-    if(this.loggedInUserDetails){
-      let changesData = JSON.parse(this.loggedInUserDetails)
-      this.currentUserName = `${changesData['firstName']} ${changesData['lastName']}`
+    if (this.loggedInUserDetails) {
+      let changesData = JSON.parse(this.loggedInUserDetails);
+      this.currentUserName = `${changesData['firstName']} ${changesData['lastName']}`;
     } else {
       this.router.navigateByUrl('');
     }
   }
 
   getValueLoop(item: any) {
-    return item
+    return item;
   }
 
   DateConvertor(oldDateFormat: any) {
-    let newDate = new Date(oldDateFormat)
-    const date = newDate.toLocaleString(undefined, { timeZone: 'Asia/Kolkata' })
+    let newDate = new Date(oldDateFormat);
+    const date = newDate.toLocaleString(undefined, {
+      timeZone: 'Asia/Kolkata',
+    });
     return date;
   }
 
   checkValueObj(obj: any, key: any) {
-    if (typeof (obj) === 'string' && obj.includes('{')) {
-      obj = JSON.parse(obj)
-      return obj
-    }
-    else {
-      if (key === "Start Time" || key === "End Time") {
-        return this.DateConvertor(obj)
+    if (typeof obj === 'string' && obj.includes('{')) {
+      obj = JSON.parse(obj);
+      return obj;
+    } else {
+      if (key === 'Start Time' || key === 'End Time') {
+        return this.DateConvertor(obj);
       } else {
-        return obj
+        return obj;
       }
     }
   }
 
   openInnerDialog(data: any, title: any) {
-    this.title = title
-    this.inputDataObj = JSON.parse(data)
+    this.title = title;
+    this.inputDataObj = JSON.parse(data);
     this.openInner = true;
   }
 
@@ -136,41 +153,43 @@ export class FormComponent implements OnInit {
     const finalObj = {
       fromDate: formatDate(this.filterForm.value.fromDate, 'dd-MM-yyyy', 'en'),
       toDate: formatDate(this.filterForm.value.toDate, 'dd-MM-yyyy', 'en'),
-      referenceId: this.filterForm.value.referenceId
+      referenceId: this.filterForm.value.referenceId,
     };
     this._invoiceService.getDataByReferenceId(finalObj).subscribe({
       next: (res: any) => {
         console.log(res);
-          this.skip = 0;
-          if(res?.success && res?.response){
-
-            this.gridData = res.response;
-
-          } else {
-            this.successTitle = 'Alert';
-            this.successMsg = res.successMsg;
-            this.openAlertDialog = true;
-            this.gridData = [];
-          }
-          this.loadItems();
+        this.skip = 0;
+        if (res?.success && res?.response) {
+          this.gridData = res.response.map((record: any) => {
+            return JSON.parse(record.responseJson);
+          });
+        } else {
+          this.successTitle = 'Alert';
+          this.successMsg = res.successMsg;
+          this.openAlertDialog = true;
+          this.gridData = [];
+          this.loading = false;
+        }
+        this.loadItems();
       },
-      error: (err: any) =>  {
+      error: (err: any) => {
+        this.loading = false;
         console.log(err?.error.response.Error);
         const error = err?.error.response.Error;
+
         this.errorMessage = Object.keys(error).map((el) => {
           return {
             header: el,
             errors: error[el],
           };
+
         });
       },
       complete: () => {
         console.info('complete');
         this.loading = false;
-      }
+      },
     });
-
-
   }
 
   public sortChange(sort: SortDescriptor[]): void {
@@ -185,19 +204,24 @@ export class FormComponent implements OnInit {
 
   private loadItems(): void {
     this.gridView = {
-      data: orderBy(this.gridData?.slice(this.skip, this.skip + this.pageSize), this.sort),
+
+      data: orderBy(
+        this.gridData?.slice(this.skip, this.skip + this.pageSize),
+        this.sort
+      ),
       total: this.gridData.length,
     };
+
   }
 
   resetFilterForm() {
     this.filterForm.reset();
   }
 
-  showResponseDialog(data:any) {
-    if(data){
+  showResponseDialog(data: any) {
+    if (data) {
       this.seletedResult = data; //JSON.parse(data);
-      this.seletedTitle = `Reference ID: ${this.seletedResult?.referenceId}`
+      this.seletedTitle = `Reference ID: ${this.seletedResult?.referenceId}`;
       this.resDialog = true;
     }
   }
@@ -208,11 +232,11 @@ export class FormComponent implements OnInit {
     this.resDialog = false;
     this.seletedResult = null;
     this.errorMessage = [];
-    this.openFailed=false;
+    this.openFailed = false;
     this.selectedError = null;
   }
 
-  closeInnerDialog(){
+  closeInnerDialog() {
     this.openInner = false;
   }
 
@@ -230,7 +254,7 @@ export class FormComponent implements OnInit {
 
   excelHeader = ['position', 'name', 'class'];
 
-  onlogout(){
+  onlogout() {
     sessionStorage.removeItem('UserInfo');
     sessionStorage.removeItem('userToken');
     sessionStorage.removeItem('userDetails');
@@ -258,5 +282,4 @@ export class FormComponent implements OnInit {
       this.openAlertDialog = true;
     }
   }
-
 }
